9a3c837414f6e329b5f7c998aae94b7d1ee4b5c8
UmarBilanov <umar.uranuulu@gmail.com>
2018-10-11 20:19:15 +0600

    extraLots

--- scrapingLotsGos.py
+++ scrapingLotsGos.py
@@ -14,8 +14,8 @@ import json
 link_page = 'http://zakupki.gov.kg/popp/view/order/view.xhtml?id='
 
 lots = []
-extralots = []
-page = link_page + '126401206'
+# data_ri = []
+page = link_page + '128014811'
 
 driver = webdriver.Firefox()
 driver.implicitly_wait(30)
@@ -23,11 +23,41 @@ driver.get(page)
 button = driver.find_element_by_link_text('RU')
 button.click()
 
+soup = BeautifulSoup(driver.page_source, 'html.parser')
+# soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+for body in soup.findAll('body'):
+    content = body.find('span', {'class': 'field-groups-view m-left m-right'})
+    for div in content.findAll('tbody', {'class': 'ui-datatable-data ui-widget-content'}):
+        last_tr = div.find_all('tr')[-1]
+        if 'data-ri' in last_tr.attrs:
+            tr_num = last_tr["data-ri"]
+last_num = int(tr_num)
+# print last_num
+d = last_num * 2 + 1
+
+x = 1
+b = str(d)
+time.sleep(6)
+
 while True:
-    try:
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
-        time.sleep(6)
-        button = driver.find_element_by_xpath('//tbody[@id=\'j_idt69:lotsTable_data\']/tr/td[8]/div')
-        button.click()
-    except TimeoutException:
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    for body in soup.findAll('body'):
+        content = body.find('span', {'class': 'field-groups-view m-left m-right'})
+        for div in content.findAll('tbody', {'class': 'ui-datatable-data'}):
+            resultText = div.prettify()
+            for row in BeautifulSoup(resultText)('tr', {'class': 'ui-expanded-row-content ui-widget-content childRowFillBG'}):
+                lots.append([row.text])
+
+    a = str(x)
+    time.sleep(6)
+    xpath = '//tbody[@id="j_idt69:lotsTable_data"]/tr[' + a + ']/td[8]/div'
+    button = driver.find_element_by_xpath(xpath)
+    button.click()
+    x += 2
+    if a == b:
         break
+
+jsonD = json.dumps(lots)
+
+print jsonD.decode('unicode_escape')
