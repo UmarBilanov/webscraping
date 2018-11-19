37a0fd8c14a1bd72b86b1f89c47bc165d4cc07f3
UmarBilanov <umar.uranuulu@gmail.com>
2018-11-19 19:01:10 +0600

    converting dates

---
+++ config.py
@@ -0,0 +1,6 @@
+MongoDB_CONFIG = {
+    'host': 'localhost',
+    'dbname': 'scrapping',
+    'colname': 'zakupki.gov.kg',
+    'port': 27017
+}
--- scrapingLotsGos.py
+++ scrapingLotsGos.py
@@ -10,8 +10,8 @@ import zipfile
 import shutil
 import gridfs
 import os
-import timestring
-from timestring import Date
+import re
+import datetime
 
 link_page = 'http://zakupki.gov.kg/popp/view/order/view.xhtml?id='
 
@@ -298,11 +298,25 @@ def inserting_to_mongoDB():
     a = gen_info[u"Дата публикации"]
     b = gen_info[u"Срок подачи конкурсных заявок"]
     d = str(a.encode('utf-8'))
+    # f = d.replace("ноября", "11")
     e = str(b.encode('utf-8'))
+
+    # replacing the string name of  month to number of month
+    rep = {"января": "01", "февраля": "02", "марта": "03", "апреля": "04", "мая": "05", "июня": "06", "июля": "07",
+           "августа": "08", "сентября": "09", "октября": "10", "ноября": "11",
+           "декабря": "12"}  # define desired replacements here
+
+    # use these three lines to do the replacement
+    rep = dict((re.escape(k), v) for k, v in rep.iteritems())
+    pattern = re.compile("|".join(rep.keys()))
+    d = pattern.sub(lambda m: rep[re.escape(m.group(0))], d)
+    e = pattern.sub(lambda m: rep[re.escape(m.group(0))], e)
+    # replacing the string name of  month to number of month
+
     print d
     print type(d)
-    PDate = timestring.Date(d)
-    FinalDate = timestring.Date(e)
+    PDate = datetime.datetime.strptime(d, "%d %m %Y %H:%M")
+    FinalDate = datetime.datetime.strptime(e, "%d %m %Y %H:%M")
     print PDate
     print FinalDate
     # converting string Date to date
@@ -339,8 +353,8 @@ def inserting_to_mongoDB():
 
     result_data = {
         "Наименование закупки": NamePurchase,
-        # "Дата публикации": Date(PDate),
-        # "Срок подачи конкурсных заявок": Date(FinalDate),
+        "Дата публикации": PDate,
+        "Срок подачи конкурсных заявок": FinalDate,
         "General info": general_data,
         "Organization info": organization_data,
         "Lots info": lots_data,
